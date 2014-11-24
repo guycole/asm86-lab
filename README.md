44e97c2f6f3bad13c4d08820c1049100bd9b529d
Guy Cole <guycole@gmail.com>
2014-11-23 16:35:09 -0800

    baby steps

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ asm86-lab
 =========
 
 ASM86 Lab
+
+* OSX 10.9.5
+* Apple Inc version cctools-855, GNU assembler version 1.38
+* NASM version 0.98.40 (Apple Computer, Inc. build 11) compiled on Sep 18 2014
+* [NASM documentation] (http://www.nasm.us/xdoc/2.11.06/html/nasmdoc0.html)
---
+++ baby_steps/hello.asm
@@ -0,0 +1,31 @@
+; hello.asm
+; 23 NOV 2012
+; http://web.archive.org/web/20070830122645/http://untimedcode.com/2007/5/20/learn-nasm-assembly-on-mac-os-x
+;
+; nasm -f macho hello.asm
+; ld -e _start -o hello hello.o
+; nasm -f macho hello.asm -l hello.lst
+;
+section	.text			; code start
+global	_start			; make externally visible
+; 
+section	.data			; data start
+	message		db	"Hello, World!", 0xa
+	message_length	equ	$-message
+; 
+_syscall:			; declare kernel call function
+	int		0x80
+	ret
+;
+_start:				; linker entry point
+				; write message to standard output
+	push	dword message_length
+	push	dword message
+	push	dword 1		; file descriptor
+	mov	eax, 0x4	; sys_write
+	call	_syscall
+	add	esp, 12		; stack cleanup
+	push	dword 0		; exit
+	mov	eax, 0x1        ; sys_exit
+	call	_syscall
+;
